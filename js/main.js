dd54b11dcd2b77c07f0afdc4f69ba53fdbc2813d
Andy Wang <awan26@ucsc.edu>
2016-05-10 17:22:51 -0700

    carried

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
 /*
 Initiate canvas
 */
-var canvas = document.getElementById('game');
+var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 /*
 Input Section
 */
 
+/* Image variables */
+
+//bg image and properties
+var bg = new Image();
+bg.src = "art/bg.png";
+
+//base image and properties
+var floor = new Image();
+floor.src = "art/build.png";
+floor.width= 400;
+floor.height= 250;
 
 /*
-Canvas draw
 Main game loop stuff
 */
 
+//Should call stuff from the working.js to grab functions that calculate damage and resource.
 function update(){
 
 }
 
-function draw(){
 
+//Show the player what they need to see
+function draw(){
+  canvas.width = canvas.width;
+  ctx.drawImage(bg,0,0, canvas.width, canvas.width); //main background
+  ctx.drawImage(floor, canvas.width-floor.width, canvas.height-floor.height, floor.width, floor.height); //building
 }
 
 function main(){
@@ -27,4 +42,4 @@ function main(){
   draw();
 }
 
-setInterval(main, 3000); //(function to repeat, milliseconds)
+setInterval(main, 300); //(function to repeat, milliseconds)
--- js/workings.js
+++ js/workings.js
@@ -3,3 +3,49 @@ Behind the Scenes
 Contains:
   - combat and health
 */
+
+var water = 0; //resource count
+
+/*
+Values correspond to the type of character
+1 : Melee (Tanky, okay dmg)
+2 : Ranged (High dmg, squishy)
+3 : Gatherer (Low dmg, used for gathering resources during daytime)
+*/
+var type = [1,2,3]; //may be unnecessary
+
+//character prototypes
+function character(name, type){
+  //if it's melee
+  if (type == 1){
+    this.name = "melee"
+    this.hp = 0;
+  }
+  //if it's ranged
+  if (type ==2){
+    this.name = "ranged";
+    this.hp = 0;
+  }
+  //if it's gatherer
+  if (type==3){
+    this.name = "gatherer";
+    this.hp = 0;
+  }
+}
+
+
+//check collisions to see if people are fighting
+function checkCombat(){
+  combat();
+}
+
+// calculate combat stuff
+// range of attacks depending on character type?
+function combat(){
+
+}
+
+//enemy generating
+function spawnEnemy(){
+
+}
