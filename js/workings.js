5489cdaeedd155237e643f814ff9dfeadf3b2e90
psychatog <van_scoik@yahoo.com>
2016-06-05 15:57:21 -0700

    test

--- js/workings.js
+++ js/workings.js
@@ -135,3 +135,55 @@ function gatherWater(unit){
     unit.act = false;
   }
 }
+
+function Sprite(opt) {
+
+    var self = {},
+        index = 0,
+        tickCount = 0,
+        ticksPerFrame = opt.ticksPerFrame || 0,
+        numFrames = opt.numFrames || 1;
+
+    self.ctx = opt.ctx;
+    self.width = opt.width;
+    self.height = opt.height;
+    self.image = opt.image;
+
+    self.update = function() {
+
+        tickCount += 1;
+
+        if (tickCount > ticksPerFrame) {
+
+            tickCount = 0;
+
+            // If the current frame index is in range
+            if (index < numFrames - 1) {
+                // Go to the next frame
+                index += 1;
+            } else {
+                index = 0;
+            }
+        }
+    };
+
+    self.draw = function() {
+
+        // Clear the canvas
+        self.ctx.clearRect(0, 0, self.width, self.height);
+
+        // Draw the animation
+        self.ctx.drawImage(
+            self.image,
+            index * self.width / numFrames,
+            0,
+            self.width / numFrames,
+            self.height,
+            0,
+            0,
+            self.width / numFrames,
+            self.height);
+    };
+
+    return self;
+}
