999af8ebf7f265c8d5b7351693a0521d5235aa7f
psychatog <van_scoik@yahoo.com>
2016-06-05 15:48:50 -0700

    fix

--- js/animate.js
+++ js/animate.js
@@ -1,55 +1,55 @@
-(function () {
 
-    function Sprite(opt) {
 
-        var self = {},
-            index = 0,
-            tickCount = 0,
-            ticksPerFrame = opt.ticksPerFrame || 0,
-            numFrames = opt.numFrames || 1;
+function Sprite(opt) {
 
-        self.ctx = opt.ctx;
-        self.width = opt.width;
-        self.height = opt.height;
-        self.image = opt.image;
+    var self = {},
+        index = 0,
+        tickCount = 0,
+        ticksPerFrame = opt.ticksPerFrame || 0,
+        numFrames = opt.numFrames || 1;
 
-        self.update = function() {
+    self.ctx = opt.ctx;
+    self.width = opt.width;
+    self.height = opt.height;
+    self.image = opt.image;
 
-            tickCount += 1;
+    self.update = function() {
 
-            if (tickCount > ticksPerFrame) {
+        tickCount += 1;
 
-                tickCount = 0;
+        if (tickCount > ticksPerFrame) {
 
-                // If the current frame index is in range
-                if (index < numFrames - 1) {
-                    // Go to the next frame
-                    index += 1;
-                } else {
-                    index = 0;
-                }
+            tickCount = 0;
+
+            // If the current frame index is in range
+            if (index < numFrames - 1) {
+                // Go to the next frame
+                index += 1;
+            } else {
+                index = 0;
             }
-        };
+        }
+    };
 
-        self.draw = function() {
+    self.draw = function() {
 
-            // Clear the canvas
-            self.ctx.clearRect(0, 0, self.width, self.height);
+        // Clear the canvas
+        self.ctx.clearRect(0, 0, self.width, self.height);
 
-            // Draw the animation
-            self.ctx.drawImage(
-                self.image,
-                index * self.width / numFrames,
-                0,
-                self.width / numFrames,
-                self.height,
-                0,
-                0,
-                self.width / numFrames,
-                self.height);
-        };
+        // Draw the animation
+        self.ctx.drawImage(
+            self.image,
+            index * self.width / numFrames,
+            0,
+            self.width / numFrames,
+            self.height,
+            0,
+            0,
+            self.width / numFrames,
+            self.height);
+    };
 
-        return self;
-    }
+    return self;
+}
 
-} ());
+
